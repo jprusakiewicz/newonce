dbe23bd1fd456a1ea637a741ae73f8fe2f5c38c0
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2021-11-20 06:20:36 +0100

    feat router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 import uvicorn
 from fastapi import FastAPI
 
-# from app.spotify import spotify_router
+from app.spotify import spotify_router
 
 app = FastAPI()
 
@@ -13,7 +13,7 @@ async def get():
     return {"status": "ok"}
 
 
-# app.include_router(spotify_router.router, prefix='/spotify')
+app.include_router(spotify_router.router, prefix='/spotify')
 #
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=5000)
